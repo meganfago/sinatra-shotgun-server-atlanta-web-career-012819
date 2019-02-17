a1f478ccfb920e7a027aae0013dd0020d41ee6e1
Megan Fago <meganlaynefago@gmail.com>
2019-02-17 21:41:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
   get '/' do 
     "Welcome to your app!!!! "
   end
-   get '/' do 
-      "Welcome to your app!!!! I BUILT THIS!"
-    end
+  get '/' do 
+    "Welcome to your app!!!! I BUILT THIS!"
+  end
 end
